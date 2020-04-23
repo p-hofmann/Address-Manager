c711b64fee5f02dce92f0e29dfccd55fcb82506d
Peter Hofmann <p.hofmann.email@gmail.com>
2020-04-23 11:10:05 +0200

    Added inital core methods of sql manager

--- CoreLib/SqlManager.cs
+++ CoreLib/SqlManager.cs
@@ -1,11 +1,92 @@
 ﻿using System;
+using System.IO;
 
 namespace CoreLib
 {
+  /// <summary>
+  /// Container of all SQLite related methods
+  /// </summary>
+  public class SqlManager
+  {
     /// <summary>
-    /// Container of all SQLite related methods
+    /// Debug mode.
+    /// 
+    /// Create temporary file at unique default path and use it as default path, so UnitTests can be run parallel without conflict.
     /// </summary>
-    public class SqlManager
+    private Boolean _debug;
+
+    /// <summary>
+    /// Default location of database.
+    /// </summary>
+    private string _defaultDbLocation;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="defaultDbLocation"></param>
+    /// <param name="debug">Debug mode</param>
+    public SqlManager(string defaultDbLocation = "./sqlDatabase", Boolean debug = false)
     {
+      _debug = debug;
+      _defaultDbLocation = defaultDbLocation;
+
+      if (debug)
+        _defaultDbLocation = Path.GetTempFileName();
     }
+
+    /// <summary>
+    /// Initialize a new, empty DB
+    /// </summary>
+    public void DbInitialize()
+    {
+      // TODO
+    }
+
+    /// <summary>
+    /// Delete DB
+    /// </summary>
+    public void DbDelete()
+    {
+      // TODO
+    }
+
+    /// <summary>
+    /// Clear DB of all entries
+    /// </summary>
+    public void DbClear()
+    {
+      // TODO
+    }
+
+    /// <summary>
+    /// Clear DB of all entries
+    /// </summary>
+    public void DbConnect()
+    {
+      // TODO
+    }
+
+    /// <summary>
+    /// Get all entries in db
+    /// </summary>
+    public void EntryGetAll()
+    {
+      // TODO
+    }
+
+    public void EntryAdd()
+    {
+      // TODO
+    }
+
+    public void EntryRemove()
+    {
+      // TODO
+    }
+
+    public void EntryModify()
+    {
+      // TODO
+    }
+  }
 }
---
+++ UnitTestCore/UnitTestSqlManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreLib;
+
+namespace UnitTestCore
+{
+  /// <summary>
+  /// UnitTests for SqlManager
+  /// </summary>
+  [TestClass]
+  public class UnitTestSqlManager
+  {
+    [TestMethod]
+    public void TestMethodSqliteDbCreate()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodSqliteDbDelete()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.DbDelete();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodSqliteDbConnect()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.DbConnect();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodEntryAdd()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.EntryAdd();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodEntryRemove()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.EntryRemove();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodEntryGetAll()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.EntryGetAll();
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    public void TestMethodEntryModify()
+    {
+      SqlManager db = new SqlManager(debug: true);
+      db.DbInitialize();
+      db.EntryModify();
+      Assert.IsTrue(false);
+    }
+  }
+}
