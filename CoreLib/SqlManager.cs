ce898b31a0306eb4620ac03ee5e366e0e4c85bff
Peter Hofmann <p.hofmann.email@gmail.com>
2020-04-23 10:28:05 +0200

    Add core sql tests

---
+++ CoreLib/SqlManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreLib
+{
+    /// <summary>
+    /// Container of all SQLite related methods
+    /// </summary>
+    public class SqlManager
+    {
+    }
+}
---
+++ UnitTestCore/UnitTestSql.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCore
+{
+    [TestClass]
+    public class UnitTestSql
+    {
+        [TestMethod]
+        public void TestMethodCreateSqliteDb()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestMethodDeleteSqliteDb()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestMethodEntryAdd()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestMethodEntryRemove()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestMethodEntryGetAll()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestMethodEntryModify()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
